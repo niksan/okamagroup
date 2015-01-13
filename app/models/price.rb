7b2b044cb9d36d10c366be62909fbf05db41cd09
NikSan <nikulinaleksandr@gmail.com>
2015-01-13 23:23:07 +0300

    fix

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,11 +1,10 @@
 class Price < ActiveRecord::Base
 
-  validates :name, presence: true
   validates :file, presence: true
   mount_uploader :file, PriceUploader
 
   def filename
-    file.path.split('/').last.split('.').first
+    file.path.split('/').last
   end
 
 end
