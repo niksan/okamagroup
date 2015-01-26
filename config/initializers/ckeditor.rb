bb877adb264ea24656d38037504ad03426b577af
NikSan <nikulinaleksandr@gmail.com>
2015-01-26 18:13:06 +0300

    ckeditor fix

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -5,6 +5,9 @@ Ckeditor.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require "ckeditor/orm/active_record"
+  Rails.application.config.assets.precompile += %w( ckeditor/* )
+  config.assets_languages = ['ru', 'en']
+  config.assets_plugins = ['image', 'smiley']
 
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
